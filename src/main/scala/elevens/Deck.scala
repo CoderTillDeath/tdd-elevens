742437747e31088c4832f1324c34da396f227289
Benson Chu <bensonchu457@gmail.com>
2017-09-20 11:00:06 -0500

    Added tests for drawing cards

--- src/main/scala/elevens/Deck.scala
+++ src/main/scala/elevens/Deck.scala
@@ -2,5 +2,12 @@ package elevens
 
 class Deck 
 {
+  var cards = 52
+  def cardsLeft() = { cards }
 
+  def drawCard() = {
+    if(cards > 0) {
+      cards -= 1
+    }
+  }
 }
--- src/test/scala/elevens/DeckTests.scala
+++ src/test/scala/elevens/DeckTests.scala
@@ -1,10 +1,30 @@
 package elevens
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite,BeforeAndAfter}
 
-class DeckTests extends FunSuite
+class DeckTests extends FunSuite with BeforeAndAfter
 {
-    test("Canary Test") {
-        assert(true)
-    }
+  var deck : Deck = _
+
+  before {
+    deck = new Deck();
+  }
+
+  test("Canary Test") {
+    assert(true)
+  }
+  
+  test("A deck of cards has 52 cards") {
+    assert(deck.cardsLeft() == 52)
+  }
+
+  test("When drawing a card from a deck, the number of cards left decreases") {
+    deck.drawCard()
+    assert(deck.cardsLeft() == 51)
+  }
+
+  test("Cannot have negative number of cards in a deck") {
+    for(i <- 1 to 53) {deck.drawCard()}
+    assert(deck.cardsLeft() == 0)
+  }
 }
